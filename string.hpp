7388ac515a57d9e7484d1af5595bd3742262f0d8
ProgramForge <2516047748@qq.com>
2026-02-10 21:19:11 +0800

    Update string.hpp

--- string.hpp
+++ string.hpp
@@ -2,6 +2,16 @@
  * @file string.hpp
  * @brief 字符串类的定义和实现
  * @details 实现了一个高效的字符串类，支持SSO（Small String Optimization）优化
+ * MIT 许可证
+ * 版权所有 (c) 2026 鼎天立地 (gitcode) 与 ProgramForge 组织 (GitHub)
+ * 特此免费授予任何获得本软件及相关文档文件（以下简称"软件"）副本的人，不受限制地处理本软件的权利，包括但不限于使用、复制、修改、合并、发布、分发、再许可和/或销售本软件的副本，并允许接受本软件的人这样做，但须符合以下条件：
+ * 上述版权声明和本许可声明应包含在本软件的所有副本或重要部分中。
+ * 本软件"按原样"提供，不提供任何形式的明示或暗示保证，包括但不限于对适销性、特定用途适用性和非侵权性的保证。在任何情况下，作者或版权持有人均不对因本软件或本软件的使用或其他交易而引起的任何索赔、损害或其他责任负责，无论是在合同诉讼、侵权诉讼还是其他诉讼中。
+ * MIT License
+ * Copyright (c) 2026 Dingtian Lidi (gitcode) and ProgramForge Organization (GitHub)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * The Software is provided "as is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose, and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the Software or the use or other dealings in the Software.
  */
 #ifndef STRING_HPP
 #define STRING_HPP
@@ -1369,4 +1379,5 @@ namespace NAMESPACE {
         return is;
     }
 }
+
 #endif // CPL_STRING_HPP
